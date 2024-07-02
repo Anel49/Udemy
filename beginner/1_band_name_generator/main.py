3292e7995644ef27e6c177ec4fa715c3ad67b94d
Anel49 <142068438+Anel49@users.noreply.github.com>
2024-07-02 19:26:19 -0400

    Update main.py
    Added the comment.

--- beginner/1_band_name_generator/main.py
+++ beginner/1_band_name_generator/main.py
@@ -2,6 +2,8 @@ print("Band Name Generator")
 city = input("\nWhat is the name of the city you grew up in?\n")
 animal = input("\nWhat is your favorite animal?\n")
 
+# custom code to fascilitate animals that are not plural.
+# don't ask me about the words geese, deer, or anything of the like...
 if animal.lower()[-1] != "s":
     animal = animal + "s"
 
